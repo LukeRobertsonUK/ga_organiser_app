7ea5fc579b2d85028e7b0fc7645cb968dc2f1bd4
Luke Robertson <luke.robertson@mac.com>
2013-07-31 13:37:36 +0100

    attempting to fix heroku

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module GaOrganiserApp
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initializer_on_preconfile = false
+    config.assets.initialize_on_preconfile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
